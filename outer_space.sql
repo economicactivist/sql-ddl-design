25b7be3632db390f8cfb1259173036c489d014a2
Mountain Scott <mountainscott@yahoo.com>
2021-06-14 20:01:25 -0700

    errors in outer space.sql

--- outer_space.sql
+++ outer_space.sql
@@ -9,20 +9,49 @@ CREATE DATABASE outer_space;
 
 CREATE TABLE planets
 (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
+  planet_name PRIMARY KEY TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
+  orbits_around_star TEXT NOT NULL,
   galaxy TEXT NOT NULL,
-  moons TEXT[]
+  UNIQUE(planet_name)
 );
 
 INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+  (planet_name, orbital_period_in_years, orbits_around_star, galaxy)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('Earth', 1.00, 'The Sun', 'Milky Way'),
+  ('Mars', 1.88, 'The Sun', 'Milky Way'),
+  ('Venus', 0.62, 'The Sun', 'Milky Way'),
+  ('Neptune', 164.8, 'The Sun', 'Milky Way'),
+  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way'),
+  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way');
+
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY,
+  moon_name TEXT NOT NULL,
+  orbits_around_planet TEXT NOT NULL,
+  CONSTRAINT orbits_around_planet FOREIGN KEY planet_name REFERENCES planets(name)
+);
+
+INSERT INTO moons
+  (moon_name, orbits_around_planet)
+VALUES
+  ('The Moon', 'Earth'),
+  ('Phobos', 'Mars'),
+  ('Deimos', 'Mars'),
+  ('Neptune', 'Naiad'), 
+  ('Thalassa', 'Neptune'), 
+  ('Despina', 'Neptune'), 
+  ('Galatea', 'Neptune'), 
+  ('Larissa', 'Neptune'), 
+  ('S/2004 N 1', 'Neptune'), 
+  ('Proteus', 'Neptune'), 
+  ('Triton', 'Neptune'), 
+  ('Nereid', 'Neptune'), 
+  ('Halimede', 'Neptune'), 
+  ('Sao', 'Neptune'), 
+  ('Laomedeia', 'Neptune'), 
+  ('Psamathe', 'Neptune'), 
+  ('Neso', 'Neptune')
+  
